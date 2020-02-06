49caf5fd61250b910077db36540285d88e68c178
junil-hwang <tjsdlf4261@naver.com>
2020-02-06 21:36:41 +0900

    front-end 프로젝트 구조 변경

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { router, store } from './middleware'
 
 Vue.config.productionTip = false
 
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+import Component from 'vue-class-component';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+@Component
+export default class Home extends Vue {
 }
 </script>
