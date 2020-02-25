017faf2844ac6b5642a3404f6288ca1e831b9616
junil-hwang <tjsdlf4261@naver.com>
2020-02-25 23:34:13 +0900

    [BE] gitignore 수정

--- back-end/src/github/secret.ts
+++
@@ -1,3 +0,0 @@
-export const client_id = '9dd231ed914f5042ccd5'
-export const client_secret = '9650ab18cec7e6268703b483ddd0bc806d9e5413'
-export const redirectURL = 'http://localhost:8080/api/github/authentication'
