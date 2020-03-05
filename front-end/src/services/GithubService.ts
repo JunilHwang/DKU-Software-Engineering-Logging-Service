38ce42391a35a4473786b8e8b339031bce1b7cb1
junil-hwang <tjsdlf4261@naver.com>
2020-03-05 10:39:56 +0900

    [FE] Github Tree API 사용으로 전환

--- front-end/src/services/GithubService.ts
+++ front-end/src/services/GithubService.ts
@@ -2,12 +2,13 @@ import $http from 'axios'
 import { Base64 } from 'js-base64'
 import { GithubRepository, GithubContent } from '@Domain/Github'
 
+const githubURL = 'https://api.github.com'
 const baseURI = '/api/github'
 
 export interface ContentVO {
   user: string
   repo: string
-  path: string
+  path?: string
 }
 
 const GithubService = class {
@@ -18,6 +19,14 @@ const GithubService = class {
 
   async getContent (params: ContentVO): Promise<GithubContent|GithubContent[]> {
     const { data } = await $http.get(`${baseURI}/content`, { params })
+    return data.result
+  }
+
+  async getTree ({ user, repo }: ContentVO): Promise<GithubContent|GithubContent[]> {
+    const { data: commits } = await $http.get(`${githubURL}/repos/${user}/${repo}/commits`)
+    const [ sha ] = commits
+    const params = { user, repo, sha }
+    const { data } = await $http.get(`${baseURI}/tree`, { params })
     return data.result
   }
 }
