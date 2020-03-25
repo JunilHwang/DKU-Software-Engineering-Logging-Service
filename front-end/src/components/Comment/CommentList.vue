bbc26601f9acb6784b57a633c4e93fd3f5f7dd32
junil-hwang <tjsdlf4261@naver.com>
2020-03-25 18:17:49 +0900

    [FE] Comment UI 작업 중 (2)

--- front-end/src/components/Comment/CommentList.vue
+++ front-end/src/components/Comment/CommentList.vue
@@ -1,14 +1,21 @@
 <template>
   <section>
-    <article v-for="{ idx, content, writer: { id, profile }, createdAt } in commentList" :key="idx">
+    <header>
+      댓글 {{ commentList.length }}
+    </header>
+    <article
+      v-for="{ idx, depth, content, writer: { id, profile: { avatar_url } }, createdAt } in commentList"
+      :key="idx"
+      :style="{ paddingLeft: `${depth * 20}px` }"
+    >
       <ul>
         <li class="commentWriter">
           <figure>
-            <img :src="profile.avatar_url" :alt="id">
+            <img :src="`${avatar_url}&s=30`" :alt="id">
           </figure>
           <span class="commentWriterID" v-html="id" />
         </li>
-        <li class="commentCreatedAt">{{ createdAt*1 | dateformat }}</li>
+        <li class="commentCreatedAt">{{ createdAt*1 | fromNow }}</li>
       </ul>
       <div class="commentContent" v-html="content" />
     </article>
@@ -36,7 +43,75 @@ export default class CommentList extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/scss/lib";
+
+ul, li, div, section, header, article, p, figure {
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  vertical-align: middle;
+}
+
+ul, li {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+
+ul {
+  font-family: enFont();
+}
+
 section {
   padding: 70px 0;
+
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 100px;
+    z-index: 10;
+    background: #f5f5f5;
+  }
 }
+
+header {
+  font-size: 21px;
+  margin-bottom: 10px;
+}
+
+article {
+  border-bottom: 1px dotted #ddd;
+  padding: 20px 0;
+}
+
+figure {
+  @include img-wrap();
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  overflow: hidden;
+}
+
+.comment {
+  &WriterID {
+    font-size: 15px;
+    margin-left: 5px;
+  }
+  &CreatedAt {
+    font-size: 13px;
+    color: #aaa;
+    margin-left: 10px;
+  }
+  &Content {
+    margin-top: 10px;
+    line-height: 1.5;
+  }
+}
+
+
 </style>
--- front-end/src/filter/index.ts
+++ front-end/src/filter/index.ts
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import moment from 'moment'
 
+moment.locale('ko')
+
 Vue.filter('dateformat', (date: Date|number|string) => moment(new Date(date)).format('YYYY-MM-DD HH:mm:ss'))
+Vue.filter('fromNow', (date: Date|number|string) => moment(new Date(date)).fromNow())
