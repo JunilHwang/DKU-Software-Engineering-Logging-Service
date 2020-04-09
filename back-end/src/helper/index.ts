b7c161220b36d5f9b422570300b3e6d44d259c96
junilHwang <tjsdlf4261@naver.com>
2020-04-09 22:55:49 +0900

    [BE] File Upload Path 변경 (로컬 디스크)

--- back-end/src/helper/index.ts
+++ back-end/src/helper/index.ts
@@ -2,7 +2,9 @@ import * as fs from 'fs'
 import { Base64 } from 'js-base64'
 import { GithubContent } from '@/domain'
 
-export const UPLOADED_PATH = `${__dirname}/../resources/uploaded`
+export const UPLOADED_PATH: string = process.env.NODE_ENV === 'development'
+                                     ? '/Users/junil/Desktop/Uploaded'
+                                     : '/data/Uploaded'
 
 export const saveBlob = (blob: string, path: string) => {
   const buffer = new Buffer(blob.split(',')[1], 'base64')
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 import { Request, Response } from 'express'
 import { join } from 'path'
 import * as express from 'express'
+import { UPLOADED_PATH } from '@/helper'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -18,7 +19,7 @@ async function bootstrap() {
   app.setBaseViewsDir(staticPath)
   app.setViewEngine('hbs')
 
-  app.use('/uploaded', express.static(`${__dirname}/resources/uploaded`))
+  app.use('/uploaded', express.static(UPLOADED_PATH))
   app.use(cookieParser())
 
   if (process.env.NODE_ENV === 'development') {
