92c534ad041f0835673a798cdfd043ab24bd4644
junilHwang <tjsdlf4261@naver.com>
2020-04-11 17:16:38 +0900

    [BE] User Controller 타입 명시

--- back-end/src/api/user/user.controller.ts
+++ back-end/src/api/user/user.controller.ts
@@ -1,7 +1,8 @@
-import {CacheTTL, Controller, Get, Query, Param, HttpCode, HttpStatus} from '@nestjs/common'
-import { UserEntity as User } from '@/entity'
+import { CacheTTL, Controller, Get, Query, Param, HttpCode, HttpStatus } from '@nestjs/common'
+import { UserEntity as User, PostViewEntity as PostView } from '@/entity'
 import { Token } from '@/middle'
 import { UserFacade } from './user.facade'
+import { GithubProfile } from '@/domain'
 
 @Controller('/api/user')
 export class UserController {
@@ -12,26 +13,26 @@ export class UserController {
   @Get()
   @HttpCode(HttpStatus.OK)
   @CacheTTL(60 * 60 * 24)
-  public async getProfile (@Query('access_token') access_token) {
+  public async getProfile (@Query('access_token') access_token): Promise<GithubProfile> {
     return await this.userFacade.findProfile(access_token)
   }
 
   @Get('/posts')
   @HttpCode(HttpStatus.OK)
-  public async getMyPosts (@Token() access_token: string) {
+  public async getMyPosts (@Token() access_token: string): Promise<PostView[]> {
     return await this.userFacade.findAllByUser({ access_token })
   }
 
   @Get('/:id/posts')
   @HttpCode(HttpStatus.OK)
-  public async getUserPosts (@Param('id') id: string) {
+  public async getUserPosts (@Param('id') id: string): Promise<PostView[]> {
     return await this.userFacade.findAllByUser({ id })
   }
 
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
   @CacheTTL(60 * 60 * 24)
-  public async getUserInfo (@Param('id') id: string) {
+  public async getUserInfo (@Param('id') id: string): Promise<User> {
     return await this.userFacade.find({ id })
   }
 }
