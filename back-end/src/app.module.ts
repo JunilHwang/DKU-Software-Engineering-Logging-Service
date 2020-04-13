82f6a7f7ac6e7bfdb03de032e79903add10547e2
황준일 <32131766@dankook.ac.kr>
2020-04-13 13:44:55 +0000

    [BE] mongo db 및 cat 제외

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,11 +1,11 @@
 import { CacheModule, Module } from '@nestjs/common'
 import ApiModules from './api'
-import { MysqlModule, MongoModule } from '@/database'
+import { MysqlModule } from '@/database'
 import { appCacheInterceptor, appResponseInterceptor, appExceptionFilter } from '@/middle'
 import { AppController } from '@/app.controller'
 
 const moduleMetaData = {
-  imports: [ ...ApiModules, MysqlModule, MongoModule, CacheModule.register() ],
+  imports: [ ...ApiModules, MysqlModule, CacheModule.register() ],
   controllers: [ AppController ],
   providers: [ appResponseInterceptor, appCacheInterceptor, appExceptionFilter ],
 }
