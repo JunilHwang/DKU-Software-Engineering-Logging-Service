d223848f2d62986403c5c5f3bf33bdb80713e64b
황준일 <32131766@dankook.ac.kr>
2020-04-13 14:00:42 +0000

    [BE] mongo db 관련 console 제거

--- back-end/src/database/mongodb/index.ts
+++ back-end/src/database/mongodb/index.ts
@@ -4,6 +4,4 @@ import { HOST, PORT, DBNAME } from './secret'
 const uri = `mongodb://${HOST}:${PORT}/${DBNAME}`;
 const options = { useNewUrlParser: true }
 
-console.log(uri)
-
 export default MongooseModule.forRoot(uri, options)
