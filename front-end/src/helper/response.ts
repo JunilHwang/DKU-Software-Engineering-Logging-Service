865cd33e14e38a14c4f778274524d80de1eb84f5
junilHwang <tjsdlf4261@naver.com>
2020-04-13 20:11:57 +0900

    [FE] API Exception 로직 수정

--- front-end/src/helper/response.ts
+++ front-end/src/helper/response.ts
@@ -1,13 +1,15 @@
 import { AxiosResponse } from 'axios'
 import { Response } from '@Domain'
 import { store } from '@/middleware'
+import { eventBus } from '../helper'
 
 export const responseProcessor = async <T>(response: Promise<AxiosResponse<Response<T>>>): Promise<T> => {
   try {
     const { data: { result } } = await response
     return result
-  } catch ({ status, response: { data: { statusCode, path } } }) {
-    if (statusCode === 401) store.commit('user/SIGN_OUT')
-    throw statusCode
+  } catch (e) {
+    if (e.status === 401) store.commit('user/SIGN_OUT')
+    eventBus.$message({ type: 'error', message: e.response.message })
+    throw e
   }
 }
